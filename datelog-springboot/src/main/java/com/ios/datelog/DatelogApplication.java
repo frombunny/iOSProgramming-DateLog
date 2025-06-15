4402b6c6acd45e56547bd0f9876f4496240bbe3c
frombunny <rucy011121@gmail.com>
2025-06-15 20:36:28 +0900

    feat: 스프링부트 프로젝트 생성

---
+++ datelog-springboot/src/main/java/com/ios/datelog/DatelogApplication.java
@@ -0,0 +1,13 @@
+package com.ios.datelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatelogApplication.class, args);
+	}
+
+}
